6905d6313bb57e0010608bb7f1d4b0b559ddb9c0
ForRest1217 <Jihan2519@naver.com>
2025-08-01 17:56:23 +0900

    DiskSafeBar

--- Assets/00.Work/WorkSpace/ForRest/02. Scripts/DiskSliderBar.cs
+++ Assets/00.Work/WorkSpace/ForRest/02. Scripts/DiskSliderBar.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class DiskSliderBar : MonoSingleton<DiskSliderBar>
 {
     [SerializeField] private Image redBar;
-    [SerializeField] private Image greenBar;
+    [SerializeField] private Image blueBar;
     [SerializeField] private RectTransform safeLine;
 
     [SerializeField] private float maxDisk;
@@ -16,6 +16,7 @@ public class DiskSliderBar : MonoSingleton<DiskSliderBar>
     {
         _disk = maxDisk;
         SetDiskSlider(_disk);
+        SafeBarPosition();
     }
 
     public void PlusDisk(float value)
@@ -47,12 +48,17 @@ public class DiskSliderBar : MonoSingleton<DiskSliderBar>
 
         redBar.fillAmount = _disk / maxDisk;
         float greenFill = Mathf.Min(_disk, diskSafe) / maxDisk;
-        greenBar.fillAmount = greenFill;
-        SafeBarPosition();
+        blueBar.fillAmount = greenFill;
     }
 
     private void SafeBarPosition()
     {
+        RectTransform redRect = redBar.rectTransform;
+        float safeRatio = diskSafe / maxDisk;
+        float localSafeX = (safeRatio - 0.5f) * redRect.rect.width;
 
+        // 레드바 로컬 기준 위치를 월드좌표로 변환
+        Vector3 worldSafePos = redRect.TransformPoint(new Vector3(localSafeX, 0f, 0f));
+        safeLine.localPosition = safeLine.parent.InverseTransformPoint(worldSafePos);
     }
 }
